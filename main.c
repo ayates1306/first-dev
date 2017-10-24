4bb30d8e7d5575b53ad859105e3532eb745be9a9
Alasdair Yates <ali1306@gmail.com>
2017-10-24 22:55:15 +0100

    Rolled back a version - ie, I submitted the file, then pulled the previous version and commit that back.

--- main.c
+++ main.c
@@ -5,6 +5,5 @@ int main(void)
   printf ("hello world\n");
   printf ("This is the second checkin\n");
   printf ("4th change - removed a file and added this\n");
-  printf ("5th change - no semicolon")
   return 0;
 }
