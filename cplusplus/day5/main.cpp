9d51378fa0dc215e7f85acfb7438bd074717022d
Alasdair Yates <ali1306@gmail.com>
2017-11-01 21:59:32 +0000

    Example of a function with multiple parameters, and multiple defaults. Note I previously tried to give defaults to only x and z but the compiler complained. I can only give defaults to parameters after non-defaulted ones, and once I have given a default, all others must have a default.

--- cplusplus/day5/main.cpp
+++ cplusplus/day5/main.cpp
@@ -2,15 +2,17 @@
 
 using namespace std;
 
-void foo(int x = 50)
+void foo(int x, int y=2, int z=3)
 {
   cout << "Foo says x is " << x << endl;
 }
 
+
+
 int main(void)
 {
   cout << "In main" << endl;
-  foo (10);
-  foo();
+  foo (10, 20, 30);
+  foo(4);
   return 0;
 }
