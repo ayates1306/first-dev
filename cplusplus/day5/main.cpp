e6d747d6e35fe3cf11237698332dcd464cd711b1
Alasdair Yates <ali1306@gmail.com>
2017-11-02 20:15:14 +0000

    first overloading exercise

--- cplusplus/day5/main.cpp
+++ cplusplus/day5/main.cpp
@@ -2,17 +2,62 @@
 
 using namespace std;
 
-void foo(int x, int y=2, int z=3)
+/* Overloading test 
+ *
+ * Overloading is where more than one function has the 
+ * same name, but different parameter lists.
+ * This is also known as polymorphism.
+ *
+ */
+
+
+int Double(int a)
 {
-  cout << "Foo says x is " << x << endl;
+  cout << "double an int" << endl;
+  return a*a;
 }
 
+long Double(long a)
+{
+  cout << "double a long" << endl;
+  return a*a;
+}
+
+float Double(float a)
+{
+  cout << "double a float" << endl;
+  return a*a;
+}
+
+double Double(double a)
+{
+  cout << "double a double" << endl;
+  return a*a;
+}
+
+using namespace std;
 
 
 int main(void)
 {
+  int i = 1, j;
+  long l = 2, m;
+  float f = 3.5, g;
+  double d = 16.667, e;
   cout << "In main" << endl;
-  foo (10, 20, 30);
-  foo(4);
+  
+  j = Double(i);
+  cout << "doubled was " << j << endl;
+
+  m = Double(l);
+  cout << "long doubled was " << m << endl;
+
+  g = Double(f);
+  cout << "float doubled was " << g << endl;
+  
+  e = Double(d);
+  cout << "double doubled was " << e << endl;
+
   return 0;
 }
+
