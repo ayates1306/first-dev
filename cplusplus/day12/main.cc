c5696871dc6be16ba62267339ec28925bbe19421
Alasdair Yates <ali1306@gmail.com>
2018-01-03 22:06:12 +0000

    Override the speak() method for a dog

--- cplusplus/day12/main.cc
+++ cplusplus/day12/main.cc
@@ -52,6 +52,9 @@ public:
   void WagTail() const { cout << "Tail wagging\n";}
   void BegForFood() const { cout << "Begging for food\n";}
 
+  // overridden speak method
+  void Speak() const {cout << "Woof!\n"; }
+
 protected:
   BREED itsBreed;
 };
@@ -60,32 +63,27 @@ Mammal::Mammal():
   itsAge(10),
   itsWeight(5)
 {
-  cout << "Mammal constructor\n";
 }
 
 Mammal::Mammal(int age):
   itsAge(age),
   itsWeight(5)
 {
-  cout << "Mammal (int) constructor\n";
 }
 
 Mammal::~Mammal()
 {
-  cout << "Mammal destructor\n";
 }
 
 Dog::Dog():
   itsBreed(GOLDEN)
 {
-  cout << "Dog constructor\n";
 }
 
 Dog::Dog(int age):
   Mammal(age),
   itsBreed(GOLDEN)
 {
-  cout << "Dog(int) constructor\n";
 }
 
 Dog::Dog(int age, int weight):
@@ -93,7 +91,6 @@ Dog::Dog(int age, int weight):
   itsBreed(GOLDEN)
 {
   itsWeight = weight;
-  cout << "Dog(int,int) constructor\n";
 }
 
 Dog::Dog(int age, int weight, BREED breed):
@@ -102,27 +99,19 @@ Dog::Dog(int age, int weight, BREED breed):
 
 {
   itsWeight = weight;
-  cout << "Dog (int, int, breed) constructor\n";
 }
 
 
 Dog::~Dog()
 {
-  cout << "Dog destructor\n";
 }
 
 int main(void)
 {
   Dog fido;
-  Dog rover(1);
-  Dog fluffy(2,3);
-  Dog rex (5,6,DOBERMAN);
-  fido.Speek();
+  fido.Speak();
   fido.WagTail();
   cout << "Fido is "<<fido.GetAge() << " years old\n";
-  cout << "Rover is "<<rover.GetAge() << " years old\n";
-  cout << "Fluffy is "<<fluffy.GetAge() << " years old\n";
-  cout << "Rex is "<<rex.GetAge() << " years old\n";
 }
 
 
