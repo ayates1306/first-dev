ec82b9ddb74e915bdc7a96bc8cecbab9bfcc2169
Alasdair Yates <ali1306@gmail.com>
2018-03-31 17:57:21 +0100

    Add group concept

--- cplusplus/led/src/main.cc
+++ cplusplus/led/src/main.cc
@@ -12,6 +12,24 @@ using namespace std;
 
 #define NUM_THREADS 5
 
+// Group
+// pattern has a group
+// start_pattern, first ask group to kill other patterns on it?
+// What about priorities on LEDs?
+//
+// Patterns represent some state
+// Patterns should be independent
+// But some patterns are grouped by function (eg, wifi, audio)
+// so changing a grouped pattern should stop any currently
+// running pattern on that group.
+// Does that mean that the APIs are:
+// add_pattern_to_group()
+// start_pattern(group, pattern)
+// As only 1 pattern can exist at one time on a group,
+// we just maintain the current pattern inside the group and
+// stop it next time start_pattern() is called.
+
+
 // class to keep track of the on/off state and the time between them
 // plus the number of repeats
 class pattern {
@@ -28,9 +46,35 @@ public:
   std::string name; 
   //  pthread_t thread_;
   void thread();
+  void start();
   std::string GetName() const { return name; }
   pthread_t hand;
 };
+
+class group {
+public:
+  group() : current_pattern(nullptr) {};
+  ~group();
+  void start_pattern(pattern*);
+private:
+  pattern* current_pattern;
+};
+
+void group::start_pattern(pattern* pattern_) {
+  if (current_pattern != nullptr) {
+    current_pattern->pattern::~pattern();
+  }
+
+  current_pattern = pattern_;
+
+  pattern_->start();
+}
+
+group::~group() {
+  if (current_pattern != nullptr) {
+    current_pattern->pattern::~pattern();
+  }
+}
 
 pattern::~pattern()
 {
@@ -74,6 +118,9 @@ pattern::pattern(int on, int off, int repeat, int delay, std::string cname):
   name(cname)
 {
   std::cout << "Create pattern named " << cname << "\n";
+}
+
+void pattern::start(void) {
   // start a thread, and remember the thread ID
   auto t = std::thread(&pattern::thread, this);
 
@@ -82,7 +129,6 @@ pattern::pattern(int on, int off, int repeat, int delay, std::string cname):
   t.detach(); //join();
 
   std::cout << "Thread presumably created\n";
-  //  while(1);
 }
 
 
@@ -93,6 +139,7 @@ static  pattern *plist[5];
 int main () {
   int i, n=0, num=0;
   bool quit=false;
+  group gp1, gp2;
 
    srand(time(NULL));
 
@@ -114,7 +161,12 @@ int main () {
      std::string name = "blah ";
      name = name + std::to_string(num);
      std::cout << "Create it called "<<name << std::endl;
-     plist[n] = new pattern(50, 50, 2, 100, name);
+     plist[n] = new pattern(250, 250, 2+n, 100, name);
+     if ((n&1) == 0){
+       gp1.start_pattern(plist[n]);
+     } else {
+       gp2.start_pattern(plist[n]);
+     }
      ++n;
      ++num;
      if (n==5) {
@@ -122,12 +174,12 @@ int main () {
      }
    }
      break;
-   case 2: {
-     n--;
-     std::cout << "Going to delete " << plist[n]->GetName() << std::endl;
-     delete plist[n];
-   }
-   break;
+     //   case 2: {
+     //     n--;
+     //     std::cout << "Going to delete " << plist[n]->GetName() << std::endl;
+     // delete plist[n];
+     //   }
+     //   break;
    default: {
    }
    break;
@@ -135,4 +187,5 @@ int main () {
    }
     
    //   pthread_exit(NULL);
+   std::cout << "End of main program\n";
 }
